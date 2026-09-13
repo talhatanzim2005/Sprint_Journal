2f8869ab39be5725354250b30b235fc52950954e
talhatanzim2005 <talhatanzim2005@gmail.com>
2026-09-13 12:26:05 +0600

    Fix errors after pulling updates

--- lib/front_end/calendar_screen.dart
+++ lib/front_end/calendar_screen.dart
@@ -18,6 +18,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
   static const Color accentColor = Color.fromARGB(255, 205, 0, 51);
   static const Color sheetBgColor = Color.fromARGB(255, 36, 36, 35);
 
+  // Selected Date & focused month
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDate = DateTime.now();
   String? _selectedItemId;
@@ -27,21 +28,25 @@ class _CalendarScreenState extends State<CalendarScreen> {
   String _dateKey(DateTime date) =>
       '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
 
+  // Getter to quickly retrieve todays item
   List<CalendarItem> get _selectedDayItems =>
       _itemsMap[_dateKey(_selectedDate)] ?? [];
 
+  // Sort items sequentially by their start time
   void _sortItems(List<CalendarItem> list) => list.sort(
     (a, b) => (a.startTime.hour * 60 + a.startTime.minute).compareTo(
       b.startTime.hour * 60 + b.startTime.minute,
     ),
   );
 
+  // Add Item
   void _addItem(CalendarItem item) => setState(() {
     final list = _itemsMap.putIfAbsent(_dateKey(item.date), () => [])
       ..add(item);
     _sortItems(list);
   });
 
+  // Edit Item
   void _editItem(CalendarItem item) => setState(() {
     final list = _itemsMap[_dateKey(item.date)];
     if (list != null) {
@@ -53,6 +58,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
     }
   });
 
+  // Delete Item WITH Snackbar
   void _deleteItem(CalendarItem item) {
     setState(
       () => _itemsMap[_dateKey(item.date)]?.removeWhere((e) => e.id == item.id),
